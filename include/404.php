439826adcb04140001c0de12c71f8b1e3abad40c
ziyodjon <44997750+ziyodjon@users.noreply.github.com>
2020-06-25 18:07:31 +0500

    В этом папке: header.php footer.php 404.php

---
+++ include/404.php
@@ -0,0 +1,2 @@
+
+<h1 align="center">Page not found 404</h1>
---
+++ include/header.php
@@ -0,0 +1,31 @@
+<?
+// Подключаю все функции
+require_once ('libs/functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contacts</title>
+    <meta name="description" content="Chartist.html">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+    <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="css/vendors.bundle.css">
+    <link id="appbundle" rel="stylesheet" media="screen, print" href="css/app.bundle.css">
+    <link id="myskin" rel="stylesheet" media="screen, print" href="css/skins/skin-master.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-solid.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-brands.css">
+    <link rel="stylesheet" media="screen, print" href="css/fa-regular.css">
+
+</head>
+<body>
+<main id="js-page-content" role="main" class="page-content">
+    <div class="subheader">
+        <h1 class="subheader-title">
+            <i class='subheader-icon fal fa-plus-circle'></i> Contacts
+            <small>
+                A simple contact page
+            </small>
+        </h1>
+
+    </div>
